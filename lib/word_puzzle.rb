87255038d13f4ac69a17cccea92577874c1472e9
Jessica Iler <inertial.observer@gmail.com>
2015-05-29 08:57:24 -0700

    pass spec to replace one vowel with a dash

--- lib/word_puzzle.rb
+++ lib/word_puzzle.rb
@@ -2,7 +2,7 @@ class String
 
   define_method(:word_puzzle) do
     pre_puzzle = self.downcase
-    # new_words = pre_puzzle.gsub(/a/, "-")
+    new_words = pre_puzzle.gsub(/a/, "-")
 
   end
 end
--- spec/word_puzzle_spec.rb
+++ spec/word_puzzle_spec.rb
@@ -4,8 +4,15 @@ require('word_puzzle')
 describe('word_puzzle') do
 
   it('changes all uppercase letters in string to lowercase') do
-    expect("Let it Rain".word_puzzle).to(eq("let it rain"))
+    expect("Let it Rain".word_puzzle).to(eq("let it r-in"))
 
   end
+
+  it('replaces all instances of the letter a with a dash symbol') do
+    expect("let it rain".word_puzzle).to(eq("let it r-in"))
+
+  end
+
+
 
 end
